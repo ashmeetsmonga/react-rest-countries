d455527fbc8c09bc4720efd2a988f1a2cb906815
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-15 23:23:45 +0530

    added pagination

--- src/components/AllCountries.jsx
+++ src/components/AllCountries.jsx
@@ -6,19 +6,33 @@ import CountryCard from "./CountryCard";
 const AllCountries = () => {
 	const fetchAllCountries = async () => {
 		const { data } = await axios.get("https://restcountries.com/v3.1/all");
-		console.log(data);
+		console.log("data fetched");
 		return data;
 	};
 
-	const [page, setPage] = useState(0);
+	const [page, setPage] = useState(1);
 
 	const { error, data, isLoading, isFetching } = useQuery("countries", fetchAllCountries);
 
 	return (
-		<div className='w-full h-full flex justify-center items-center gap-4 bg-gray-800'>
-			{data.slice(page, page + 4)?.map((country, idx) => (
+		<div className='w-full h-full flex justify-center items-center gap-4 bg-gray-800 p-8'>
+			<button
+				className='text-white text-6xl font-[800] disabled:text-gray-600'
+				disabled={page === 1}
+				onClick={() => setPage((prev) => prev - 1)}
+			>
+				{"<"}
+			</button>
+			{data.slice((page - 1) * 4, page * 4)?.map((country, idx) => (
 				<CountryCard key={idx} country={country} />
 			))}
+			<button
+				className='text-white text-6xl font-[800] disabled:text-gray-600'
+				disabled={page === 125}
+				onClick={() => setPage((prev) => prev + 1)}
+			>
+				{">"}
+			</button>
 		</div>
 	);
 };
--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const CountryCard = ({ country }) => {
 	return (
-		<div className='flex flex-col bg-gray-700 rounded-md'>
-			{/* <img className='h-1/2 object-cover rounded-t-md' src={country.flags.svg} alt='' /> */}
+		<div className='flex flex-col bg-gray-700 rounded-md w-full'>
+			<img className='h-[12rem] object-cover rounded-t-md' src={country.flags.svg} alt='' />
 			<div className='flex flex-col gap-4 mt-8 px-4'>
 				<div className='font-[600] text-white text-xl'>{country.name.official}</div>
-				<div className='flex flex-col gap-0.5'>
+				<div className='flex flex-col gap-0.5 pb-4'>
 					<div className='text-white text-sm'>
 						Population : <span className='text-gray-400'>{country.population}</span>
 					</div>
