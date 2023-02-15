eec1d739c2bcd66711c9ebc1e38e4d8d0aa73b92
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-15 23:29:04 +0530

    minor ui change in card

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -5,7 +5,7 @@ const CountryCard = ({ country }) => {
 		<div className='flex flex-col bg-gray-700 rounded-md w-full'>
 			<img className='h-[12rem] object-cover rounded-t-md' src={country.flags.svg} alt='' />
 			<div className='flex flex-col gap-4 mt-8 px-4'>
-				<div className='font-[600] text-white text-xl'>{country.name.official}</div>
+				<div className='font-[600] text-white text-xl'>{country.name.common}</div>
 				<div className='flex flex-col gap-0.5 pb-4'>
 					<div className='text-white text-sm'>
 						Population : <span className='text-gray-400'>{country.population}</span>
